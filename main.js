1c9ed3f57682b676e635d95f8cadac57631d7f5a
Matt Gill <mtgill90@outlook.com>
2019-03-23 10:05:58 -0500

    form revealed when button pressed

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hi');
+// Print to DOM function 
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+startSortingButton = document.getElementById('start-sorting');
+const sortDiv = document.getElementById('sort-form'); 
+sortDiv.style.visibility = 'hidden';
+
+const sortFormUnhide = (divId) => {
+    sortDiv.style.visibility = 'visible';
+};
+
+startSortingButton.addEventListener('click', sortFormUnhide);
+
