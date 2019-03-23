2429abf5b19f1ecd85990bf8bc1da106b45ee7ab
Matt Gill <mtgill90@outlook.com>
2019-03-23 11:10:53 -0500

    objects adding to array

--- main.js
+++ main.js
@@ -4,13 +4,48 @@ const printToDom = (divId, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
 };
 
-startSortingButton = document.getElementById('start-sorting');
+const sortFormButton = document.getElementById('start-sorting');
 const sortDiv = document.getElementById('sort-form'); 
 sortDiv.style.visibility = 'hidden';
 
+
+//funtion to unhide sort-form div
 const sortFormUnhide = (divId) => {
     sortDiv.style.visibility = 'visible';
 };
 
-startSortingButton.addEventListener('click', sortFormUnhide);
+sortFormButton.addEventListener('click', sortFormUnhide);
+
+const sortButton = document.getElementById('sort-form-button');
+let students = [];
+const houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
+let studentId = 0;
+
+
+// function to build student card
+const addStudent = () => {
+    const studentName = document.getElementById('nameInput').value;
+    const chosenHouse = getHouse(houses);
+    studentId++;
+    students.unshift({name: studentName, 
+                   house: chosenHouse,
+                   id: studentId});
+    document.getElementById('nameInput').value = "";
+    console.log(students);
+};
+
+
+
+
+// function that assigns student a random house 
+const getHouse = (array) => {
+    const houseIndex = Math.floor((Math.random() * 4));
+    const studentHouse = houses[houseIndex];
+    return studentHouse;
+};
+
+sortButton.addEventListener('click', addStudent);
+
+
+
 
