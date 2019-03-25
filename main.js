510efe615294405536e3187009687e9f65fdf8c0
Matt Gill <mtgill90@outlook.com>
2019-03-25 17:15:17 -0500

    color cards working, text in name field working, form building dynamically

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+
+let students = [];
+const houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
+let studentId = 0;
+
+
+
 // Print to DOM function 
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
@@ -6,7 +13,7 @@ const printToDom = (divId, textToPrint) => {
 
 const sortFormButton = document.getElementById('start-sorting');
 const sortDiv = document.getElementById('sort-form'); 
-sortDiv.style.visibility = 'hidden';
+// sortDiv.style.visibility = 'hidden';
 
 
 //funtion to unhide sort-form div
@@ -14,29 +21,39 @@ const sortFormUnhide = (divId) => {
     sortDiv.style.visibility = 'visible';
 };
 
-sortFormButton.addEventListener('click', sortFormUnhide);
+const buildForm = () => {
+   let domString = "";
+   domString += `<div id="sort-form">`;
+   domString += `<h3>Enter Student Name</h3>`;
+   domString += `<form class='form-inline'>`;
+   domString += `<div class="form-group">`;
+   domString += `<label for="nameInput" class="studentNameLabel">Student Name:</label>`;
+   domString += `<input type="text" class="form-control" id="nameInput" placeholder="Enter name">`;
+   domString += `</div>`;
+   domString += `<button type="submit" class="btn btn-primary" id="sort-form-button">Sort!</button>`;
+   domString += `</form>`;
+   domString += `</div>`;
+   printToDom('form-div', domString);
+   const sortButton = document.getElementById('sort-form-button');
+   sortButton.addEventListener('click', addStudent);
+}
 
-const sortButton = document.getElementById('sort-form-button');
-let students = [];
-const houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
-let studentId = 0;
 
 //function to build student card
 const studentBody = () => {
     domString = '';
 
-    students.forEach((student) => {
-    domString += `<div class="col-2" id='${student.id}'>`;    
-    domString += `<div class="card">`;
-    domString += `<div class="card-body">`;
+    students.forEach(student => {
+    domString += `<div class="col-12 col-sm-6 col-md-3">`;    
+    domString += `<div class="card ${student.house}" id='${student.id}'>`;
     domString += `<h5 class="card-title">${student.name}</h5>`;
-    domString += `<p class="card-text">${student.house}</p>`;
-    domString += `<a href="#" class="btn btn-primary" id='expel-button-${student.id}'>Expel!</a>`;
+    domString += `<h5 class="card-text">${student.house}</h5>`;
+    domString += `<button type="button" class="btn btn-secondary expel" id='expel'>Expel!</button>`;
     domString += `</div>`;
     domString += `</div>`;
-    domString += `</div>`;
+    
     });
-    printToDom('student-body-div', domString);
+    return domString;
 };
 
 
@@ -44,13 +61,20 @@ const studentBody = () => {
 const addStudent = () => {
     const studentName = document.getElementById('nameInput').value;
     const chosenHouse = getHouse(houses);
+
+    if (studentName === ""){
+        alert('To proceed please enter a name');
+    }
+    else{
     studentId++;
     students.unshift({name: studentName, 
                    house: chosenHouse,
-                   id: studentId});
+                   id: studentId.toString(10)});
     studentBody();
     document.getElementById('nameInput').value = "";
-    console.log(students);
+    printToDom('student-body-div', domString);
+    expelEvent()
+    }
 };
 
 
@@ -63,10 +87,51 @@ const getHouse = (array) => {
     return studentHouse;
 };
 
+//assigns event listeners to expel buttons 
+const expelEvent = () => {
+    
+    let expelButton = document.getElementsByClassName('expel');
+
+    for (let button of expelButton){
+        button.addEventListener('click', function () {removeStudent(button.parentElement.id)});
+    }
+  
+}
 
 
+const findStudentById = (id) => {
+    let idValue = '';
+    students.forEach(student => {
+        if (student.id === id) {
+            idValue = students.indexOf(student);
+        }
+    });
+    return idValue;
+}
 
-sortButton.addEventListener('click', addStudent);
+//removes student from the array using the id value from findStudentsById
+const removeStudent = (id) => {
+        
+        let studentIndex = findStudentById(id);
+        students.splice(studentIndex, 1);
+        printToDom('student-body-div', studentBody());
+        expelEvent();
+    };
+
+    
+const buttonEvents = () => {
+    
+    sortFormButton.addEventListener('click', buildForm);
+}
+
+
+const init = () => {
+    buttonEvents();
+}
+
+init();
+
+
 
 
 
